86952f21e2eb34f1b8f8178d6028f2f244b28bba
notebook <xiegaofeng@foxmail.com>
2019-03-20 14:27:01 +0800

    demo

---
+++ request.js
@@ -0,0 +1,16 @@
+var request = new Object();
+
+request.wxRequest = function(obj){
+  
+  wx.request({
+    url: 'http://test.com/LKT/index.php?module=api&software_name=3&edition=1.0'+obj.url,
+    data:obj.data,
+    method:obj.method,
+    header:{"Content-Type": "application/x-www-form-urlencoded"},
+    success:function(res){
+        typeof(obj.success)=='function' && obj.success(res.data);    
+    }
+  })
+
+}
+module.exports = request
